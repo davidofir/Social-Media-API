49cc62ac8ac0b28674a9b9e6928ebb9eded9b695
ofir <davidof@sheridancollege.ca>
2021-11-21 01:07:29 -0500

    added post request

--- Assignment 2 - WeblyAPI/API/Controllers/UsersController.cs
+++ Assignment 2 - WeblyAPI/API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using API.Models.Entities;
 using API.Models.Persistence;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,15 +16,27 @@ namespace API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
-        [HttpGet("/{id}")]
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
         //api/users/{id}
         public async Task<IActionResult> GetUserById(string id)
         {
             var user = await _context.Users.FindAsync(new Guid(id));
+
             if (user != null)
                 return Ok(user);
             else
                 return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
--- Assignment 2 - WeblyAPI/API/Models/Entities/User.cs
+++ Assignment 2 - WeblyAPI/API/Models/Entities/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace API.Models.Entities
     {
         public Guid Id { get; set; }
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
         public List<Image> Images { get; set; }
     }
