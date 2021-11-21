934b81e4c946ef69fdc022fe78855ef92fece4b3
ofir <davidof@sheridancollege.ca>
2021-11-21 00:56:11 -0500

    getuserby added

--- Assignment 2 - WeblyAPI/API/Controllers/UsersController.cs
+++ Assignment 2 - WeblyAPI/API/Controllers/UsersController.cs
@@ -2,14 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using API.Models.Persistence;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+
     public class UsersController : ControllerBase
     {
-
+        private readonly DataContext _context;
+        [HttpGet("/{id}")]
+        //api/users/{id}
+        public async Task<IActionResult> GetUserById(string id)
+        {
+            var user = await _context.Users.FindAsync(new Guid(id));
+            if (user != null)
+                return Ok(user);
+            else
+                return NotFound();
+        }
     }
 }
