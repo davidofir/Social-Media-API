83e195763919afe8fbe57455c43d828e4f715769
ofir <davidof@sheridancollege.ca>
2021-11-27 22:00:33 -0500

    addition of images resolved

--- Assignment 2 - WeblyAPI/API/Controllers/UsersController.cs
+++ Assignment 2 - WeblyAPI/API/Controllers/UsersController.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using API.Models.Entities;
+using API.Models.Helpers;
 using API.Models.Persistence;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
@@ -43,9 +46,31 @@ namespace API.Controllers
             {
                 return BadRequest();
             }
-            await _context.AddAsync(user);
+            await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return Ok(user);
+        }
+        [HttpPost("{id}/image")]
+        public async Task<IActionResult> AddImageToUser(string id, [FromBody] Image img)
+        {
+            var user = await _context.Users.FindAsync(new Guid(id));
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            var tagsList = ImageHelper.GetTags(img.Url);
+            img.User = user;
+            img.Tags = new List<Tag>();
+            foreach (var tg in tagsList)
+            {
+                img.Tags.Add(new Tag
+                {
+                    Text = tg
+                });
+            }
+            _context.Images.Add(img);
+            await _context.SaveChangesAsync();
+            return Ok(img);
         }
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string id)
